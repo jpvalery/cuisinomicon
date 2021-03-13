b928e96a4bbf103a1337647b0ca7c6a633f43643
jpvalery <jpvalery@users.noreply.github.com>
2021-03-12 19:48:15 -0500

    fix: header

--- components/Header.js
+++ components/Header.js
@@ -10,24 +10,24 @@ export default function Header() {
   const [isMobileOpen, setIsMobileOpen] = useState(false);
   return (
     <div className="sticky top-0 z-50 max-w-3xl pt-4 mx-auto bg-gradient-to-b from-eigengrau-600">
-      <div className="flex justify-between py-6 border-b-2 border-gray-800">
+      <div className="flex items-center justify-between p-4 border-b-2 border-gray-800">
         <NextLink href="/">
-          <div className="inline-flex items-center w-8 h-8 ml-6 text-base font-medium outline-none cursor-pointer md:w-10 md:h-10 text-main-500 group hover:text-main-100">
+          <div className="w-8 h-8 text-base font-medium outline-none cursor-pointer md:w-10 md:h-10 text-main-500 group hover:text-main-100">
             <CookBook />
             <span className="sr-only">SiteName</span>
           </div>
         </NextLink>
 
-        <div className="flex">
+        <div className="grid items-center grid-flow-col gap-6">
           <NextLink href="/manger">
-            <div className="inline-flex items-center w-8 h-8 ml-6 text-base font-medium outline-none cursor-pointer md:w-10 md:h-10 text-manger-500 group hover:text-manger-100">
+            <div className="w-8 h-8 text-base font-medium outline-none cursor-pointer md:w-10 md:h-10 text-manger-500 group hover:text-manger-100">
               <CuttingBoard />
               <span className="sr-only">Manger</span>
             </div>
           </NextLink>
 
           <NextLink href="/manger">
-            <div className="inline-flex items-center w-8 h-8 ml-6 text-base font-medium outline-none cursor-pointer md:w-10 md:h-10 text-boire-500 group hover:text-boire-100">
+            <div className="w-8 h-8 text-base font-medium outline-none cursor-pointer md:w-10 md:h-10 text-boire-500 group hover:text-boire-100">
               <Lemonade />
               <span className="sr-only">Boire</span>
             </div>
